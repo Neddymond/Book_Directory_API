c99d8fd3992752f246f746089cf0b62389b3b4ee
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-09-05 01:15:15 +0100

    Creates an endpoint for rating a book

--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -1,4 +1,4 @@
-const Book = require("../models/Books");
+const Book = require("../models/Book");
 const Rating = require("../models/Rating");
 const express = require("express");
 const router = new express.Router();
@@ -7,12 +7,14 @@ const auth = require("../Middleware/auth");
 /** Endpoint for adding books */
 router.post("/books/add", auth, async (req, res) => {
   try {
-    const book = new Book({ ...req.body });
+    const book = new Book({
+      ...req.body
+    });
 
     // Enter book in the db
     await book.save();
 
-    res.status(201).send({ Books });
+    res.status(201).send({ book });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -52,20 +54,85 @@ router.get("/books/all", async (req, res) => {
       res.status(404).send("Directory currently empty");
     }
 
-    // get all the ratings for this book
-    const ratings = await Rating.find({ _id: books.ratings });
+    let sum_of_all_ratings;
 
-    const sum_of_all_ratings;
-    for (let i = 0; i <= ratings.length; i++) {
-      sum_of_all_ratings += ratings[i].value;
+    for (let i = 0; i <= books.ratings.length; i++) {
+      sum_of_all_ratings += books.ratings[i];
     }
 
     // calculate the average rating by dividing the sum of all ratings by the number of ratings
-    const averageRating = sum_of_all_ratings / ratings.length;
+    const averageRating = (sum_of_all_ratings / book.ratings.length).toFixed(1);
 
-    res.send({ Books, averageRating });
+    res.send({ books, averageRating });
   } catch (e) {
     res.status(500).end(e);
+  }
+});
+
+router.post("/books/rate/:id", auth, async (req, res) => {
+ try {
+  const book = await Book.findById(req.params.id);
+
+  // Check if a user rating already exists on a book
+  const existingRating = await Rating.findOne({
+    userId: req.user._id,
+    bookId: book._id
+  });
+
+  // If a rating already exists, update it. Otherwise, create a new rating.
+  if (existingRating) {
+    existingRating["rating"] = req.body.rating;
+    await existingRating.save();
+
+    res.send(existingRating);
+  } else {
+    const rating = new Rating({
+      rating: req.body.rating,
+      userId: req.user._id,
+      bookId: book._id
+    });
+
+    await rating.save();
+    // console.log(rating);
+  
+    const bookRating = book.ratings.push(rating._id);
+    await book.save();
+
+    res.send(rating);
+  }
+ } catch (e) {
+   res.status(500).send(e);
+ }
+});
+
+/** Endpoint for updating a Book */
+router.patch("/books/update/:id", auth, async (req, res) => {
+  try {
+    // All attributes we want to modify
+    const reqBody = Object.keys(req.body);
+
+    // Properties we can update
+    const updateables = ["name", "authors", "pages"];
+
+    // Make sure we are only updating updateable properties
+    const isUpdateable = reqBody.every((property) => updateables.includes(property));
+
+    if (!isUpdateable) {
+      return res.status(400).send({ Error: "Invalid update"});
+    }
+
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      res.status(404).send()
+    }
+
+    reqBody.forEach((property) => book[property] = req.body[property]);
+
+    await book.save();
+
+    res.send(book);
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
@@ -75,7 +142,7 @@ router.delete("/books/del", auth, async (req, res) => {
     // delete all books
     const deletedBooks = await Book.find({}).deleteMany();
 
-    res.send(Books);
+    res.send(deletedBooks);
   } catch (e) {
     res.status(500).send(e);
   }
