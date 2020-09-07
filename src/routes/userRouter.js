c6343803d065f97f4aecd516ea503a9a2051d6d8
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-09-07 05:54:05 +0100

    modified user model path

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 /** User Model */
-const User = require("../Models/User");
+const User = require("../models/User");
 
 /** Auth Middleware */
 const auth = require("../Middleware/Auth");
